530064a2895f56288750e2b6f9893a6472e21fed
dereekb <dereekb@gmail.com>
2015-02-07 16:03:09 -0600

    Added MultipleMapTest Board
    - Found issue related to relative position of elements. Added TODO to
    complete later.

--- Biters/Assets/Biters Assets/Scripts/Debug/Game/TestGameBoard.cs
+++ Biters/Assets/Biters Assets/Scripts/Debug/Game/TestGameBoard.cs
@@ -43,6 +43,62 @@ namespace Biters.Testing
 		}
 
 	}
+	
+	/*
+	 * Multiple Map Board.
+	 */
+	public class MultipleMapTestGameBoard : MonoBehaviour
+	{
+		private TestMapGenerator TestMapGenerator;
+		private GameMap<BitersGameTile, BitersMapEntity> Map;
+		private GameMap<BitersGameTile, BitersMapEntity> MapB;
+		private GameMap<BitersGameTile, BitersMapEntity> MapC;
 		
+		public MultipleMapTestGameBoard () {}
+		
+		public void Awake()
+		{
+			Console.WriteLine ("Waking up...");
+			
+			TestMapGenerator TestMapGenerator = new TestMapGenerator();
+			
+			GameObject MapObject = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+			MapObject.transform.localScale = new Vector3 (0.2f, 0.2f, 0.2f);
+			IGameMapDelegate<BitersGameTile, BitersMapEntity> MapDelegate = TestMapGenerator.Make ();
+			
+			//TODO: Issue of "Now Game Tiles NEED to have this map set before they are created, but the map also needs a generator first..."
+			this.Map = new GameMap<BitersGameTile, BitersMapEntity> (MapObject, MapDelegate);
+			
+			/*
+			 * TODO: Need to ensure that map objects use local position 
+			 * (position relative to the map, as it is the Transform Parent) so maps can move all together.
+			 * 
+			GameObject MapBObject = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+			MapBObject.transform.localScale = new Vector3 (0.2f, 0.2f, 0.2f);
+
+			IGameMapDelegate<BitersGameTile, BitersMapEntity> MapBDelegate = TestMapGenerator.Make ();
+			this.MapB = new GameMap<BitersGameTile, BitersMapEntity> (MapBObject, MapBDelegate);
+
+			Debug.Log ("Creating new world...");
+
+			Map.ResetWorld ();
+			MapB.ResetWorld ();
+
+			MapBObject.transform.position = new Vector3 (-20f, -20f, -4f);
+			*/
+			
+			Debug.Log ("Finished resetting world...");
+		}
+		
+		public void Update ()
+		{
+			Debug.Log ("Update.");
+			//Debug.Log ("Updating: %s", Time.time);
+			Map.Update ();
+			MapB.Update ();
+		}
+		
+	}
+
 }
 
