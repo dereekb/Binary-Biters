e043845ec06e29b78a2c124fe6a3b1d2eb6e645c
dereekb <dereekb@gmail.com>
2015-02-27 00:15:32 -0600

    Added Scripts to Menus
    - Updated Menus

---
+++ Biters/Assets/Biters Assets/Scripts/Menus/HomeMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using Biters.Menus.WorldEditor;
+
+namespace Biters.Menus {
+
+	public class HomeMenu : MonoBehaviour {
+
+		public static readonly string MAIN_MENU_SCENE_NAME = "HomeScene";
+
+		public static void LoadLevel() {
+			Application.LoadLevel (MAIN_MENU_SCENE_NAME);
+		}
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		public void SegueToMapEditor () {
+			BitersWorldEditor.LoadLevel ();
+		}
+
+	}
+
+}
---
+++ Biters/Assets/Biters Assets/Scripts/Menus/World Editor/WorldEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Biters.Menus.WorldEditor {
+
+	public class BitersWorldEditor : MonoBehaviour {
+
+		public static readonly string MAP_EDITOR_SCENE_NAME = "WorldEditorScene";
+		
+		public static void LoadLevel() {
+			Application.LoadLevel (MAP_EDITOR_SCENE_NAME);
+		}
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+	}
+
+}
